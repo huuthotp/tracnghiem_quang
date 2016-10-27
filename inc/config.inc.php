9608df42a32f5872990896fac0ab1a9290ef5e0f
huuthotp <huutho.tp@gmail.com>
2016-10-27 13:27:36 +0700

    source code master

---
+++ inc/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+$srv_settings['title_postfix'] = 'WEBSITE THI TRẮC NGHIỆM TRỰC TUYẾN';
+$srv_settings['default_email'] = 'buiducquang@gmail.com';
+$srv_settings['language'] = 'vn';
+$srv_settings['db_driver'] = 'mysql';
+$srv_settings['db_host'] = 'localhost';
+$srv_settings['db_db'] = 'u516137763_quang';
+$srv_settings['db_user'] = 'u516137763_quang';
+$srv_settings['db_password'] = 'Quang@123';
+$srv_settings['table_prefix'] = 'tracnghiem_';
+$srv_settings['url_root'] = '/';
+$srv_settings['dir_root_full'] = '/public_html/';
+$srv_settings['url_files'] = '/files/';
+$srv_settings['dir_files_full'] = '/public_html/files/';
+$srv_settings['version'] = 'da1b3e84b64bad53f9008754d26470db';
+?>
