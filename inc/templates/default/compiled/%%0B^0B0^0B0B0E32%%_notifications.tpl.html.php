3ff30c3970cb4aba46f0c18a8cb7190320078567
huuthotp <huutho.tp@gmail.com>
2016-10-31 11:45:44 +0700

    delete file tmp

---
+++ inc/templates/default/compiled/%%0B^0B0^0B0B0E32%%_notifications.tpl.html.php
@@ -0,0 +1,6 @@
+<?php /* Smarty version 2.6.18, created on 2016-10-31 10:54:04
+         compiled from _notifications.tpl.html */ ?>
+<?php if ($this->_tpl_vars['g_vars']['page']['errors']): ?><p><b><font color="#cc0000"><?php echo $this->_tpl_vars['g_vars']['page']['errors']; ?>
+</font></b><?php endif; ?>
+<?php if ($this->_tpl_vars['g_vars']['page']['notifications']): ?><p><b><font color="#006000"><?php echo $this->_tpl_vars['g_vars']['page']['notifications']; ?>
+</font></b><?php endif; ?>
