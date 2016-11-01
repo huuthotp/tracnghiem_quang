b75c2e1e0db3b7f8dbc644cb40042321e7164a28
huuthotp <huutho.tp@gmail.com>
2016-11-01 17:12:19 +0700

    fix math jax

--- shared/MathJax/jax/output/HTML-CSS/jax.js
+++ shared/MathJax/jax/output/HTML-CSS/jax.js
@@ -357,7 +357,7 @@
 							"min-height" : 0,
 							border : 0,
 							padding : 0,
-							margin : 10px
+							margin : 10
 						},
 						".MathJax:focus, body :focus .MathJax" : {
 							display : "inline-table"
